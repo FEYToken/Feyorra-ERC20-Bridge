2b5b25cbbe2b902d73f8a35326fe34f70b83d61c
Feyorra <hi@feyorra.com>
2024-11-28 15:59:31 +0200

    refactor: remove nonce variable initialization in RandomBytes32Generator contract

--- contracts/RandomBytes32Generator.sol
+++ contracts/RandomBytes32Generator.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.24;
 
 abstract contract RandomBytes32Generator {
-    uint256 private nonce = 0;
+    uint256 private nonce;
 
     function generateRandomBytes32() internal returns (bytes32) {
         nonce++;
